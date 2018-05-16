a1d0b8f613d918d8bad94c3366db570a609ce243
Liping Ruan <hello@ruanmail.com>
2018-05-16 19:03:46 +0800

    Update versions

--- time.js
+++ time.js
@@ -31,20 +31,20 @@ function dateFormat(timestamp, formatStr) {
     let sec = date.getSeconds();
     let ms = date.getMilliseconds();
 
-    str = str.replace(/yyyy|YYYY/, fullYear);
-    str = str.replace(/yy|YY/, (year % 100) > 9 ? (year % 100).toString() : '0' + (year % 100));
-    str = str.replace(/MM/, (month + 1) > 9 ? (month + 1).toString() : '0' + (month + 1));
+    str = str.replace(/yyyy|YYYY/g, fullYear);
+    str = str.replace(/yy|YY/g, (year % 100) > 9 ? (year % 100).toString() : '0' + (year % 100));
+    str = str.replace(/MM/g, (month + 1) > 9 ? (month + 1).toString() : '0' + (month + 1));
     str = str.replace(/M/g, (month + 1));
     str = str.replace(/w|W/g, Weeks[week]);
-    str = str.replace(/dd|DD/, day > 9 ? day.toString() : '0' + day);
+    str = str.replace(/dd|DD/g, day > 9 ? day.toString() : '0' + day);
     str = str.replace(/d|D/g, day);
-    str = str.replace(/hh|HH/, hour > 9 ? hour.toString() : '0' + hour);
+    str = str.replace(/hh|HH/g, hour > 9 ? hour.toString() : '0' + hour);
     str = str.replace(/h|H/g, hour);
-    str = str.replace(/mm/, minu > 9 ? minu.toString() : '0' + minu);
+    str = str.replace(/mm/g, minu > 9 ? minu.toString() : '0' + minu);
     str = str.replace(/m/g, minu);
-    str = str.replace(/ss|SS/, sec > 9 ? sec.toString() : '0' + sec);
+    str = str.replace(/ss|SS/g, sec > 9 ? sec.toString() : '0' + sec);
     str = str.replace(/s|S/g, sec);
-    str = str.replace(/xxx|XXX/, (ms > 9 ? ms > 99 ? '' : '0' : '00') + ms);
+    str = str.replace(/xxx|XXX/g, (ms > 9 ? ms > 99 ? '' : '0' : '00') + ms);
     return str
 }
 
