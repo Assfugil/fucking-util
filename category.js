4d78faf42880320564ada5109965518214b0640d
Liping Ruan <hello@ruanmail.com>
2018-11-29 10:50:09 +0800

    [Category] extendsof support single arg

--- category.js
+++ category.js
@@ -18,6 +18,13 @@ class Category {
 
   static extendsof(cA, cB) {
 
+    if ( !cB ) {
+
+      cB = cA;
+
+      cA = this;
+    }
+
     return cA.__proto__ === cB ? true : 
            cA.__proto__ === null ? false : 
            Category.extendsof(cA.__proto__, cB);
